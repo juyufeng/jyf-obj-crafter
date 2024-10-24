b4c87898739429a813cfee48dbfbb085aadecdba
juyufeng <345108530@qq.com>
2024-10-24 15:36:15 +0800

    V1.2.0

--- JyfObjCrafter.js
+++ JyfObjCrafter.js
@@ -16,7 +16,8 @@
   }
 })(this, () => {
   'use strict';
-
+  
+  // 预备使用
   function kksTp(value) {
     if (value === null) return 'null';
     if (value === undefined) return 'undefined';
@@ -24,11 +25,11 @@
   }
 
   // 模仿abstract 我们在2010年的时候都这样写,没有类 现在2024年了
-  function JyfObj() {}
+  // function JyfObj() {}
 
   // 如果 base 或 top 对象通过原型链继承了属性，这些继承的属性不会被复制到 target 对象中。
   // 此外，如果 base 和 top 有同名属性，top 的属性值会覆盖 base 的属性值。
-  JyfObj.prototype.topFuckBase = function (base, top)  {
+  ObjectMergerType1 = function (base, top) {
     const target = {};
     [base, top].forEach(obj => {
       Object.keys(obj).forEach(prop => {
@@ -41,7 +42,7 @@
   };
 
   const JyfObjCrafter = {
-    JyfObj
+    ObjectMergerType1
   };
   return JyfObjCrafter;
 });
