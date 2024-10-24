bf5a2089680e672186409f0ee834cf7a41128d78
juyufeng <345108530@qq.com>
2024-10-24 15:36:55 +0800

    ...

--- ejs/JyfObjCrafter.js
+++ ejs/JyfObjCrafter.js
@@ -17,6 +17,7 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
 })(void 0, function () {
   'use strict';
 
+  // 预备使用
   function kksTp(value) {
     if (value === null) return 'null';
     if (value === undefined) return 'undefined';
@@ -24,11 +25,11 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
   }
 
   // 模仿abstract 我们在2010年的时候都这样写,没有类 现在2024年了
-  function JyfObj() {}
+  // function JyfObj() {}
 
   // 如果 base 或 top 对象通过原型链继承了属性，这些继承的属性不会被复制到 target 对象中。
   // 此外，如果 base 和 top 有同名属性，top 的属性值会覆盖 base 的属性值。
-  JyfObj.prototype.topFuckBase = function (base, top) {
+  ObjectMergerType1 = function ObjectMergerType1(base, top) {
     var target = {};
     [base, top].forEach(function (obj) {
       Object.keys(obj).forEach(function (prop) {
@@ -40,7 +41,7 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
     return target;
   };
   var JyfObjCrafter = {
-    JyfObj: JyfObj
+    ObjectMergerType1: ObjectMergerType1
   };
   return JyfObjCrafter;
 });
--- es6/JyfObjCrafter.js
+++ es6/JyfObjCrafter.js
@@ -1,5 +1,6 @@
 
 
+  // 预备使用
   function kksTp(value) {
     if (value === null) return 'null';
     if (value === undefined) return 'undefined';
@@ -7,11 +8,11 @@
   }
 
   // 模仿abstract 我们在2010年的时候都这样写,没有类 现在2024年了
-  function JyfObj() {}
+  // function JyfObj() {}
 
   // 如果 base 或 top 对象通过原型链继承了属性，这些继承的属性不会被复制到 target 对象中。
   // 此外，如果 base 和 top 有同名属性，top 的属性值会覆盖 base 的属性值。
-  JyfObj.prototype.topFuckBase = function (base, top) {
+  ObjectMergerType1 = function ObjectMergerType1(base, top) {
     var target = {};
     [base, top].forEach(function (obj) {
       Object.keys(obj).forEach(function (prop) {
@@ -23,6 +24,6 @@
     return target;
   };
   var JyfObjCrafter = {
-    JyfObj: JyfObj
+    ObjectMergerType1: ObjectMergerType1
   };
 export default JyfObjCrafter;
